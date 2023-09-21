10742c5b1c818ad1a264999628f54c37eb94514f
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-21 17:25:18 -0300

    update Arquivos de estudo C/SaidaDeDados.c

---
+++ Arquivos de estudo C/SaidaDeDados.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main{
+	
+	int idade;
+	double salario;
+	char sexo;
+	char nome[10]
+	
+	nome[10]= "Thesly Silva";
+	idade= 21;
+	sexo= 'M';
+	salario= 400.50;
+
+printf("%s",nome);
+printf("%i",idade);
+printf("%c",sexo);
+printf("%d",salario);
+	
+}
