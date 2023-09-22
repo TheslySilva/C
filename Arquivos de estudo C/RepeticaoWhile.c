a3c8607c81b68e1b601e8c010b39e81f64c2fd11
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-22 12:26:34 -0300

    update Arquivos de estudo C/RepeticaoWhile.c

--- Arquivos de estudo C/RepeticaoWhile.c
+++ Arquivos de estudo C/RepeticaoWhile.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-int main(){
-	
-	int x=0;
+int main() {
+
+	int x = 0;
 	int i;
-	
+
 	printf("Digite um numero : ");
 	scanf("%i", &i);
-	
-	while(i!=x){
-		
-		printf("Digite outro numero : ")
-		scanf("%i",i);
-		
-		
+
+	while(i != x) {
+
+		printf("Digite outro numero : ");
+		scanf("%i", &i);
+
 	}
-	
+return 0;
 }
