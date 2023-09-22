50c2b6fba3612262b52d2c67fca46725749416ed
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-22 12:24:41 -0300

    update Arquivos de estudo C/RepeticaoWhile.c

---
+++ Arquivos de estudo C/RepeticaoWhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	int x=0;
+	int i;
+	
+	printf("Digite um numero : ");
+	scanf("%i", &i);
+	
+	while(i!=x){
+		
+		printf("Digite outro numero : ")
+		scanf("%i",i);
+		
+		
+	}
+	
+}
