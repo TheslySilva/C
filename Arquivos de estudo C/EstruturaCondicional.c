941e6e046ae073919dc06aeabbc4367adefcf29d
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-22 08:49:08 -0300

    update Arquivos de estudo C/EstruturaCondicional.c

---
+++ Arquivos de estudo C/EstruturaCondicional.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main{
+	
+	int dia;
+	
+	printf("Que horas sao? ");
+	scanf("%i", &dia);
+	
+	if(dia>=6 && dia <=12){
+		
+		printf("Bom dia!\n")
+		
+	}else if(dia > 12 && dia <18){
+		
+	printf("Boa Tarde!\n");
+	
+	}else if(dia >=18 && dia <=23){
+		printf("Boa Noite!");
+	}else{
+		printf("Boa Madrugada!")
+	}
+	return 0;
+}
