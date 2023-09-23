9b87b196a72db5b7b162395016123434d17731da
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 09:45:00 -0300

    update Arquivos de estudo C/Idades.c

--- Arquivos de estudo C/Idades.c
+++ Arquivos de estudo C/Idades.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	
+int main() {
+
 	char nome1[50];
 	double idade1;
-	
+
 	char nome2[50];
 	double idade2;
-	
+
 	double media;
-	
+
 	//Primera Pessoa
-	
+
 	printf("Dados da primeira pessoa :\n");
 
 	printf("Nome : ");
-	fgets(nome1,50,stdin);
-	
+	fgets(nome1, 50, stdin);
+
 	printf("Idade : ");
-	scanf("%lf",&idade1);
-	
+	scanf("%lf", &idade1);
+
 	getchar();
-	
+
 	//Segunda Pessoa
-	
+
 	printf("Dados da primeira pessoa :\n");
 
 	printf("Nome : ");
-	fgets(nome2,50,stdin);
-	
+	fgets(nome2, 50, stdin);
+
 	printf("Idade : ");
-	scanf("%lf",&idade2);
-	
+	scanf("%lf", &idade2);
+
 	//Resultado
-	
-	media= (idade1+idade2)/2;
-	printf("\nA idade media entre %s e %s é de %.1lf anos",nome1,nome2,media);
-	
+
+	media = (idade1+idade2)/2;
+	printf("\nA idade media entre %s e %s é de %.1lf anos", nome1, nome2, media);
+
 	return 0;
 }
