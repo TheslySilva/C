148c4c6817a257fd327e203746bb7276bc4232ca
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 12:02:49 -0300

    update Arquivos de estudo C/MenorDeTres.c

---
+++ Arquivos de estudo C/MenorDeTres.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n1,n2,n3,x;
+	
+	printf("Digite primeiro numero : ");
+	scanf("%d",&n1);
+	printf("Digite segundo numero : ");
+	scanf("%d",&n2);
+	printf("Digite terceiro numero : ");
+	scanf("%d",&n3);
+	
+	if(n1<n2 && n1< n3){
+		
+		printf("\nMenor Numero = %d",n1);
+		
+	}else if(n2<n1 && n2<n3){
+		
+		printf("\nMenor Numero = %d",n2);
+		
+	}else if(n3<n1 && n3<n2){
+		
+		printf("\nMenor Numero = %d",n3);
+		
+	}else{
+    printf ("%d",n1);
+    }
+	
+	return 0;
+}
