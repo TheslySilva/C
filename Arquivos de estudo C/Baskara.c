d902d80b4f26382110b6cbbf7c2da34dcd07abff
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 10:47:50 -0300

    update Arquivos de estudo C/Baskara.c

---
+++ Arquivos de estudo C/Baskara.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+	
+	double a,b,c,delta;
+	double x,x1,x2;
+	
+	printf("Coeficiente a : ");
+	scanf("%lf", &a);
+	printf("Coeficiente b : ");
+	scanf("%lf",&b);
+	printf("Coeficiente c : ");
+	scanf("%lf",&c);
+	
+	delta= pow(b,2.0)-4.0*a*c;
+    
+    if(a==0|| delta<0){
+    
+  printf("\nESTA EQUACAO NAO POSSUI RAIZES REAIS!");
+    
+    }else{
+    
+	x1= (double)(+(-b)+sqrt(delta))/(2*a);
+	x2= (double)(+(-b)-sqrt(delta))/(2*a);
+	printf("\nRESULTADO :\n");
+	printf("\nX1 = %.4lf",x1);
+	printf("\nx2 = %.4lf",x2);
+    }
+	return 0;
+}
