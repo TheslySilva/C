3c6cf02d8edb1a5fe3b0abbf7c47e1bbd687fe04
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 21:43:17 -0300

    update Arquivos de estudo C/Diagonal.c

--- Arquivos de estudo C/Diagonal.c
+++ Arquivos de estudo C/Diagonal.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+
+	int x,
+	y,
+	n;
 	
-	int x,y,n;
 	printf("Digite a ordem da matriz : ");
-	scanf("%d",&n);
-	
+	scanf("%d", &n);
+
 	int matriz[n][n];
-	
-	for(x=0;x<n;x++){
-		
-		for(y=0;y<n;y++){
-			printf("\nElemento [%d,%d] : ",x,y);
+
+	for(x = 0; x < n; x++) {
+
+		for(y = 0; y < n; y++) {
+			
+			printf("\nElemento [%d,%d] : ", x, y);
 			scanf("%d", &matriz[x][y]);
 			
 		}
 	}
 	printf("\nDIAGONAL PRINCIPAL:\n");
-	for(x=0;x<n;x++){
-		
-		printf("%d ",matriz[x][x]);
-	}
 	
+	for(x = 0; x < n; x++) {
+		
+	printf("%d ", matriz[x][x]);
+	}
 	return 0;
 }
