5430497022efe676ed6ea880e8cb5b4b3a255c55
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 21:21:48 -0300

    update Arquivos de estudo C/Alturas.c

--- Arquivos de estudo C/Alturas.c
+++ Arquivos de estudo C/Alturas.c
@@ -2,51 +2,53 @@
 
 int main() {
 
-    int i,n;
+	int i,
+	n;
 
-    printf("Quantas pessoas voce vai digitar? ");
-    scanf("%d", &n);
+	printf("Quantas pessoas voce vai digitar? ");
+	scanf("%d", &n);
 
-    getchar();
+	getchar();
 
-    char nomes[n][50];
-    int idades [n];
-    double alturas [n];
-    double x=0;
-    double somaAlturas=0;
+	char nomes[n][50];
+	int idades [n];
+	double alturas [n];
+	double x = 0;
+	double somaAlturas = 0;
 
-    for(i=0; i<n; i++) {
+	for(i = 0; i < n; i++) {
 
-        printf("\nDigite os dados da %d° pessoa :",i+1);
+		printf("\nDigite os dados da %d° pessoa :", i+1);
 
-        printf("\nNome = ");
-        fgets(nomes[i],50,stdin);
+		printf("\nNome = ");
+		fgets(nomes[i], 50, stdin);
 
-        printf("Idade = ");
-        scanf("%d",&idades[i]);
+		printf("Idade = ");
+		scanf("%d", &idades[i]);
 
-        printf("Altura = ");
-        scanf("%lf", &alturas[i]);
-        somaAlturas= somaAlturas+ alturas[i];
+		printf("Altura = ");
+		scanf("%lf", &alturas[i]);
+		somaAlturas = somaAlturas+ alturas[i];
 
-        if(idades[i]<16) {
-            x++;
-        }
+		if(idades[i] < 16) {
+			x++;
+		}
 
-        getchar();
+		getchar();
 
-    }
-    double p= (x*100.0)/n;
+	}
+	double p = (x*100.0)/n;
 
-    double mediaAlturas=(double) somaAlturas/n;
+	double mediaAlturas = (double) somaAlturas/n;
 
-    printf("\nAltura media = %.2lf",mediaAlturas);
-    printf("\nPessoas com menos de 16 anos = %.1lf%",p);
-    printf("\nQuem sao = ");
-    for(i=0; i<n; i++) {
-        if(idades[i]<16) {
-            printf("%s ",nomes[i]);
-        }
-    }
-    return 0;
+	printf("\nAltura media = %.2lf", mediaAlturas);
+	printf("\nPessoas com menos de 16 anos = %.1lf%",p);
+	printf("\nQuem sao = ");
+	
+	for(i = 0; i < n; i++) {
+		if(idades[i] < 16) {
+			printf("%s ", nomes[i]);
+		}
+	}
+	return 0;
 }
